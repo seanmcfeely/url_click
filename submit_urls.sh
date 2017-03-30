f406f11661aadf17e9f7c6e88cd046b5b4ef85c5
Sean McFeely <sean.mcfeely@integraldefense.com>
2017-03-30 14:57:14 -0400

    initial commit

---
+++ submit_urls.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /opt/url_click/
+
+rm results.log.old
+mv results.log results.log.old
+
+# go back 35 minutes as it takes ~ 10-30 minutes for CB results
+# to get indexed and appear in splunk. \_(-_-)_/ 
+date=$(date -d '75 minutes ago' +'%Y-%m-%d %H:%M:%S')
+end_date=$(date -d '15 minutes ago' +'%Y-%m-%d %H:%M:%S')
+
+printf "*************\n\tStarting work at $(date +'%Y-%m-%d %H:%M:%S')\n*************\n" > results.log
+
+export -n http_proxy && export -n https_proxy && /opt/splunklib/splunk.py -i -c 'splunk.ini' -s "$date" -e "$end_date" --search-file url_clicks.search --json | jq -r '.result[].clicked_url' | while read url; do printf "\nURL: "; echo "$url"; curl -k "https://cloudphish1.local:5000/saq/cloudphish/submit?url=$url&a=1" 2> /dev/null; done >> results.log
+
+printf "\n*************\n\tCompleted at $(date +'%Y-%m-%d %H:%M:%S')\n*************\n" >> results.log
